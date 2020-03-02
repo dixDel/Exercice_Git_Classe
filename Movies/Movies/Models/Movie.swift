096187cfc3b5355806f2836c6c343c7c244f99fd
Alain20100 <alain20100@gmail.com>
2020-03-02 14:56:50 +0100

    WebService protocol

---
+++ Movies/Movies/Models/Movie.swift
@@ -0,0 +1,17 @@
+//
+//  Movie.swift
+//  Movies
+//
+//  Created by student5304 on 02/03/2020.
+//  Copyright © 2020 student5310. All rights reserved.
+//
+
+import Foundation
+
+struct Movie: Codable {
+  var title: String?
+  var releaseDate: String?
+  var posterUrl: String?
+  var overview: String?
+  var director: String?
+}
