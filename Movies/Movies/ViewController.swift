a692cb97b8391b846911a4dd0006196177564da7
fpantalone <57344139+fpantalone@users.noreply.github.com>
2020-03-02 15:02:31 +0100

    Feat: TableView with search textview and button

--- Movies/Movies/ViewController.swift
+++ Movies/Movies/ViewController.swift
@@ -9,12 +9,37 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var movieTableView: UITableView!
+    @IBOutlet weak var searchButton: UIButton!
+    
+    @IBOutlet weak var searchTextView: UITextField!
     override func viewDidLoad() {
+        
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        setupTableview()
     }
 
+    func setupTableview (){
+       
+        movieTableView.delegate = self
+        movieTableView.dataSource = self
+        movieTableView.register(UINib(nibName: "MovieCell", bundle: nil), forCellReuseIdentifier: "movieCellID")
+        
+    }
 
 }
 
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let movieCell = movieTableView.dequeueReusableCell(withIdentifier: "movieCellID", for: indexPath) as! MovieCell
+        
+        return movieCell
+    }
+    
+    
+}
