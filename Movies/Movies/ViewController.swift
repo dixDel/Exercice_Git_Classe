22326cabab8e7c291c77c9b03ea35d6563a64984
Didier Delhaye <didier.delhaye@drkdidel.be>
2020-03-02 14:08:06 +0100

    init project

---
+++ Movies/Movies/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Movies
+//
+//  Created by student5310 on 02/03/2020.
+//  Copyright © 2020 student5310. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
