dd5416e612a551f710224db8c78caf1d5474698e
Mopatissier <41237228+Mopatissier@users.noreply.github.com>
2020-03-02 14:43:14 +0100

    View Controler detail first design

---
+++ Movies/Movies/DetailViewController.swift
@@ -0,0 +1,20 @@
+//
+//  DetailViewController.swift
+//  Movies
+//
+//  Created by student5306 on 02/03/2020.
+//  Copyright © 2020 student5310. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
