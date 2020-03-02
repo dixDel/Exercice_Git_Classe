63465da4523b08855a6ef1a95f29c54fff208225
student5308 <student5308@tfns5308.local>
2020-03-02 15:14:18 +0100

    feat: Starting implementation of FakeMovieRequest

---
+++ Movies/Movies/Network/FakeMovieRequest.swift
@@ -0,0 +1,23 @@
+//
+//  FakeMovieRequest.swift
+//  Movies
+//
+//  Created by student5308 on 02/03/2020.
+//  Copyright © 2020 student5310. All rights reserved.
+//
+
+import Foundation
+
+class FakeMovieRequest: MovieRequest {
+    
+    
+    func getList(_ title: String, page: Int) -> MoviePage? {
+        <#code#>
+    }
+    
+    func getImage(_ url: String, size: Int) -> UIImage? {
+        <#code#>
+    }
+    
+    
+}
