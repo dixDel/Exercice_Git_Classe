8601db6f83cedc68eec08ed909ddbda555bc386d
student5308 <student5308@tfns5308.local>
2020-03-02 16:03:40 +0100

    rebase

--- Movies/Movies/Network/MovieRequest.swift
+++ Movies/Movies/Network/MovieRequest.swift
@@ -12,11 +12,6 @@ typealias MovieRequestCallback = (_ page: MoviePage?, _ error: String?) -> Void
 typealias ImageRequstCallback = (_ image: UIImage, _ error: String?) -> Void
 
 protocol MovieRequest {
-<<<<<<< HEAD
   func getList (_ title: String, page: Int?, complationHandler: @escaping MovieRequestCallback) -> Void
   func getImage (_ url: String, size: Int, complationHandler: @escaping ImageRequstCallback) -> Void
-=======
-  func getList (_ title: String, page: Int?) -> MoviePage?
-  func getImage (_ url: String, size: Int) -> UIImage?
->>>>>>> update Protocol + Movie
 }
